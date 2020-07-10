9f8e2758ecb93709664c0d6d3170b81d18efb2d1
Alexandre Chanson <a.chanson.tours@gmail.com>
2020-07-10 15:27:58 +0200

    custom metric

--- exp_clustering.py
+++ exp_clustering.py
@@ -18,28 +18,31 @@ print("Running tensor computations on", device)
 
 U, sigma, Vt, all_actual_ratings, all_user_predicted_ratings, movies_df, ratings_df, films_nb = load_data()
 
+CLS_MOVIE_WEIGHT = 2
 
-for movie_id in [12,954,231,45,69]:
-    print(" --- movie id", movie_id, "--- ")
+for MOVIE_ID in [12, 954, 231, 45, 69]:
+    print(" --- movie id", MOVIE_ID, "--- ")
     # build interp space with lasso
     other_movies = list(range(all_actual_ratings.shape[1]))
-    other_movies.remove(movie_id)
+    other_movies.remove(MOVIE_ID)
     LX = np.nan_to_num(all_actual_ratings[:, other_movies])
-    Ly = np.nan_to_num(all_user_predicted_ratings[:, movie_id])
+    Ly = np.nan_to_num(all_user_predicted_ratings[:, MOVIE_ID])
 
     # LARS learning
     # complexity of the model is defined by N_FEATS: 30 features + intercept
-
     reg = linear_model.Lars(fit_intercept=True, n_nonzero_coefs=15)
     reg.fit(LX, Ly)
 
     # select indexes of non 0 coefficients to determine the reduced space
-    indexes = np.argwhere(reg.coef_ != 0).T.flatten()
+    indexes = list(np.argwhere(reg.coef_ != 0).T.flatten())
     n_dim_int = np.sum(reg.coef_ != 0)
 
     # build distance matrix for clustering
     # TODO -> add current film with higher wheight
-    dist = pdist(np.nan_to_num(all_actual_ratings)[:, list(indexes)])
+    metric_indexes = indexes + [MOVIE_ID]
+    w = np.ones(len(metric_indexes))
+    w[len(indexes)] = float(CLS_MOVIE_WEIGHT)
+    dist = pdist(np.nan_to_num(all_actual_ratings)[:, metric_indexes], 'wminkowski', p=2, w=w)
 
 
     linked = linkage(dist, 'ward')
@@ -50,13 +53,13 @@ for movie_id in [12,954,231,45,69]:
 
     rootnode, nodelist = to_tree(linked, rd=True)
 
-    for user_id in [75]:#[75,158,231,459]:
+    for USER_ID in [75]:#[75,158,231,459]:
 
-        path = path_from_root(rootnode, nodelist[user_id])
+        path = path_from_root(rootnode, nodelist[USER_ID])
         cluster_ids = pick_cluster(path)
-        cluster_ids.remove(user_id)
+        cluster_ids.remove(USER_ID)
 
-        print("user id", user_id, "cluster size", len(cluster_ids))
+        print("user id", USER_ID, "cluster size", len(cluster_ids))
 
         n_neighbors = len(cluster_ids)
 
@@ -64,7 +67,7 @@ for movie_id in [12,954,231,45,69]:
         v = torch.tensor(Vt, device=device, dtype=torch.float32)
 
         # 1. Generate perturbations in interpretable space
-        base_user = torch.tensor(np.nan_to_num(all_actual_ratings[user_id, other_movies]), device=device, dtype=torch.float32)
+        base_user = torch.tensor(np.nan_to_num(all_actual_ratings[USER_ID, other_movies]), device=device, dtype=torch.float32)
         # here the interpretable space is a reduction of the initial space based on indexes
         base_user_int = base_user[reg.coef_ != 0]
 
@@ -74,7 +77,7 @@ for movie_id in [12,954,231,45,69]:
         # 2. generate perturbations in original space
         i = 0
         for pu in pert_int:
-            pert_orr[i] = torch.tensor(np.nan_to_num(all_actual_ratings[user_id]), device=device, dtype=torch.float32)
+            pert_orr[i] = torch.tensor(np.nan_to_num(all_actual_ratings[USER_ID]), device=device, dtype=torch.float32)
             j = 0
             for index in indexes:
                 pert_orr[i][index] = pert_int[i][j]
@@ -92,11 +95,11 @@ for movie_id in [12,954,231,45,69]:
         for i in range(10):
             model = LinearRecommender(n_dim_int)
 
-            l = loss.LocalLossMAE_v3(torch.tensor(np.nan_to_num(all_actual_ratings[user_id]), device=device, dtype=torch.float32), map_fn=lambda _: pert_orr, sigma=0.3)
+            l = loss.LocalLossMAE_v3(torch.tensor(np.nan_to_num(all_actual_ratings[USER_ID]), device=device, dtype=torch.float32), map_fn=lambda _: pert_orr, sigma=0.3)
 
-            train(model, pert_int, y_orr[:, movie_id], l, 100, verbose=False)
+            train(model, pert_int, y_orr[:, MOVIE_ID], l, 100, verbose=False)
 
             gx_ = model(base_user_int).item()
-            fx = get_fx(torch.tensor(np.nan_to_num(all_actual_ratings[user_id]), device=device, dtype=torch.float32), s, v, films_nb)[0,movie_id].item()
+            fx = get_fx(torch.tensor(np.nan_to_num(all_actual_ratings[USER_ID]), device=device, dtype=torch.float32), s, v, films_nb)[0, MOVIE_ID].item()
             print("mae", abs(gx_ - fx))
 
