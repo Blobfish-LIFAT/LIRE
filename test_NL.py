33e988e67fbc9d0c5416765e500bda66a35b9de1
nicolas labroche <nicolas.labroche@univ-tours.fr>
2020-07-23 11:14:55 +0200

    test_NL contains a test to validate that the range of scipy.spatial.distance.cosine is [0,2] which is perfect in our case. Test goes up to 10000 dimensions

--- test_NL.py
+++ test_NL.py
@@ -4,13 +4,38 @@ import numpy as np
 from math import sqrt, pow
 from numpy.linalg import norm
 from utility import load_data
+import random as rd
+from scipy.spatial.distance import cosine
 
+"""
 user = torch.tensor([1, 0, 4, 0, 0, 0, 2])
-#res = perturbations_3(user)
+res = perturbations_3(user)
 res = perturbations_swap(user, 10)
 print(res)
+"""
 
+# test cosine distance
+def test_cosine_dist(nb_tests):
+    dist = np.zeros((nb_tests))
+    for i in range(nb_tests):
+        # setting a number of dimensions
+        dim = rd.randint(1, 10000)
+        # generating vectors
+        u = np.random.randint(-5, 5, dim)
+        v = np.random.randint(-5, 5, dim)
 
+        dist[i] = cosine(u, v)
+        if dist[i] < 0:
+            print("We might have a problem (d < 0)")
+        elif dist[i] > 1:
+            print("I spotted a distance greater than 1! (but that's expected)")
+        elif dist[i] > 2:
+            print("We might have a problem (d > 2)")
+    print("done")
+
+test_cosine_dist(1000)
+
+"""
 def epsilon_neighborhood_fast(R, uindx, epsilon):
     # base similarity matrix (all dot products)
     # replace this with A.dot(A.T).toarray() for sparse representation
@@ -69,7 +94,7 @@ for user in range(610):
     rob = robustness(np.nan_to_num(all_actual_ratings[user]), all_user_predicted_ratings[user, MOVIE_ID], np.nan_to_num(all_actual_ratings[voisins]), all_user_predicted_ratings[voisins, MOVIE_ID])
     print(" ", "Robustness", rob)
 
-
+"""
 
 
 
